5290f7382494af9177053021c7349bd2461905e9
Matias Kuoppala <matias.kuoppala@aalto.fi>
2022-02-13 01:03:25 +0200

    Record button press commands instead of tuples. Add better user instructions.

--- mrecorder.py
+++ mrecorder.py
@@ -1,5 +1,6 @@
 from pynput import keyboard, mouse
 from pynput.keyboard import Key
+from pynput.mouse import Button
 import time
 import os
 
@@ -12,53 +13,75 @@ class Recorder():
         self.holding_key = []
         self.holding_mouse = []
         self.m_listener = mouse.Listener(on_click=self.on_click)
+        self.recording = False
+        print("Press 'r' again to start recording. Press esc to stop recording.")
+
+    # Format action times into intervals between each action.
+    # Exclude the last action which is always pressing esc.
+    def format_times(self):
+        length = len(self.actions)
+        actions_formatted = []
+        for i in range(len(self.actions) - 1):
+            cur_time = self.actions[i][1]
+            next_time = self.actions[i + 1][1]
+            actions_formatted.append((self.actions[i][0], next_time - cur_time))
+        return actions_formatted
 
     def on_press(self, key):
-        # Stop and save when esc pressed
-        if key == keyboard.Key.esc:
-            self.save()
-            self.m_listener.stop()
-            return False
-        elif key not in self.holding_key:
-            self.actions.append(("keyboard", key, "press", time.time()))
+        if key not in self.holding_key and self.recording:
+            self.actions.append(("self.keyboard.press("+str(key)+")", time.time()))
             self.holding_key.append(key)
 
     def on_release(self, key):
-        if key in self.holding_key:
-            self.holding_key.remove(key)
-        self.actions.append(("keyboard", key, "release", time.time()))
+        # Stop and save when esc pressed
+        if key == keyboard.Key.esc:
+            self.m_listener.stop()
+            return False
+        elif not self.recording:
+            if key == keyboard.KeyCode.from_char('r'):
+                self.recording = True
+        elif self.recording:
+            if key in self.holding_key:
+                self.holding_key.remove(key)
+            self.actions.append(("self.keyboard.release("+str(key)+")", time.time()))
 
     def on_click(self, x, y, button, pressed):
-        p = ""
-        if pressed:
-            if button in self.holding_mouse:
-                pass
+        if self.recording:
+            if pressed:
+                if button in self.holding_mouse:
+                    pass
+                else:
+                    self.holding_mouse.append(button)
+                    self.actions.append(("self.mouse.press("+str(button)+")", time.time()))
             else:
-                p = "press"
-                self.holding_mouse.append(button)
-        else:
-            if button in self.holding_mouse:
-                self.holding_mouse.remove(button)
-            p = "release"
-        self.actions.append(("mouse", button, "release", time.time()))
+                if button in self.holding_mouse:
+                    self.holding_mouse.remove(button)
+                    self.actions.append(("self.mouse.release("+str(button)+")", time.time()))
+            
 
-    def start(self):
+    def run(self):
         self.m_listener.start()
         with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as k_listener:
             k_listener.join()
-    
+
+        actions_formatted = self.format_times()
+        self.save(actions_formatted)
+
     # Save action list into txt
-    def save(self):
+    def save(self, actions):
+        print("Recording finished.")
         name = input("Enter macro name: ") + ".txt"
         f = open(name, "w")
         print("Saving macro..")
         # TODO: Ask "do you want to overwrite?" for already existing files
-        for action in self.actions:
+        
+        for action in actions:
             for a in action:
                 f.write(str(a))
                 f.write(',')
             f.write('\n')    
         print("Macro saved as " + name + ".")
+
 
 # Play macro from given file
 class Player():
@@ -68,24 +91,26 @@ class Player():
         self.mouse = mouse.Controller()
         self.macro = file
 
-    def play(self):
-        prev_time = 0
-        for line in self.macro:
-            action = tuple(map(str, line.strip().split(',')))
-            if action[0] == "keyboard":
-                if action[2] == "press":
-                    eval("self.keyboard.press(" + action[1] + ")")
-                elif action[2] == "release":
-                    eval("self.keyboard.release(" + action[1] + ")")
-            elif action[0] == "mouse":
-                if action[2] == "press":
-                    eval("self.mouse.press(" + action[1] + ")")
-                elif action[2] == "release":
-                    eval("self.mouse.release(" + action[1] + ")")
-            if prev_time != 0:
-                time.sleep(float(action[3]) - prev_time)
-            prev_time = float(action[3])
+        print("Press 'p' again to play macro.")
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as k_listener:
+            k_listener.join()
 
+    def on_press(self, key):
+        pass
+
+    def on_release(self, key):
+        if key == keyboard.KeyCode.from_char('p'):
+           self.play()
+           return False
+
+    def play(self):
+        for line in self.macro:
+            action = tuple(line.strip().split(','))
+            print(action)
+            print(action[0])
+            eval(action[0])
+            time.sleep(float(action[1]))
+        
 
 # Program entry point
 if __name__ == "__main__":
@@ -93,7 +118,7 @@ if __name__ == "__main__":
         action = input("Record macro ('r') or play macro ('p')? : ")
         if action == 'r':
             recorder = Recorder()
-            recorder.start()
+            recorder.run()
         elif action =='p':
             chosen = False
             while not chosen:
@@ -108,7 +133,8 @@ if __name__ == "__main__":
                         filename = user_input + ".txt"
                         file = open(filename, 'r')
                         player = Player(file)
-                        player.play()
                         chosen = True
                     except FileNotFoundError as e:
                         print(e)
+
+# TODO: Figure out why macro is not playing currently
